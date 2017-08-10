3f852f17666b8410855917ce4613fe37cf9d63b9
Dan Schwartz <schwartz@substantial.com>
2017-08-10 15:42:32 -0700

    Scaffold header & products

---
+++ redFishBlueFish/client/src/Header.js
@@ -0,0 +1,12 @@
+// @flow
+import React from 'react'
+import './header.css'
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>Red Fish Blue Fish</h1>
+      <h3>You make the kids, we make the clothes</h3>
+    </header>
+  )
+}
